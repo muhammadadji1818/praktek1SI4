55c086f2878b2e437d5063d254352c18b529ff45
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-03-26 19:07:25 +0800

    membuat menu nasigoreng

--- src/praktek1si4/nasigoreng.java
+++ src/praktek1si4/nasigoreng.java
@@ -10,5 +10,19 @@ package praktek1si4;
  * @author akagami
  */
 public class nasigoreng {
+    String bumbu_masak;
+    String peralatan_masak;
+    Integer diskon_harga, harga_nasigoreng;
+    String kreasi_nasigoreng;
+    
+    void menu_nasigoreng(){
+        system.out.println("Bumbu masak :"+bumbu_masak);
+        system.out.println("Peralatan masak :"+peralatan_masak);
+        system.out.println("diskon harga :"+diskon_harga);
+        system.out.println("harga nasigoreng :"+harga_nasigoreng);
+        system.out.println("kreasi nasigoreng :"+kreasi_nasigoreng);
+              
+    }
+    
     
 }
