0c69b2a7054c1be6e78e08e1e8829ba4b8f8eac5
muhammadadji1818 <35833891+muhammadadji1818@users.noreply.github.com>
2018-03-26 19:34:39 +0800

    mengisi nilai atribut

--- src/praktek1si4/nasigorengsuper.java
+++ src/praktek1si4/nasigorengsuper.java
@@ -13,6 +13,11 @@ public class nasigorengsuper {
     public static void main(String[] args) {
         nasigoreng ekstra_pedas = new nasigoreng();
         
-    
+       ekstra_pedas.bumbu_masak = "reyco garam cabe rawit ";
+        ekstra_pedas.peralatan_masak = "wajan kompor spatulla ";
+        ekstra_pedas.diskon_harga = 5;
+        ekstra_pedas.harga_nasigoreng = 10000 ;
+        ekstra_pedas.kreasi_nasigoreng = "bentuk yang unik dan sederhana ";
+       
     
 }
